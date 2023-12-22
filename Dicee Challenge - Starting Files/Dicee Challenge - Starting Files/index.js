e6e88fe33efee201928baa7afca5aa2efe79be88
Maryem <chakrounmeryem23@gmail.com>
2023-12-22 17:06:01 +0100

    Update index.js

--- Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
@@ -1,8 +1,34 @@
-const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomDiceImage1="dice"+randomNumber1+".png";
+var randomImageSource1="images/"+randomDiceImage1;
+
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource1);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomDiceImage2="dice"+randomNumber2+".png";
+var randomImageSource2="images/"+randomDiceImage2;
+
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+
+
+
+
+
+
+
+
+
+
+
+
+/*const randomNumber1 = Math.floor(Math.random() * 6) + 1;
 const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 console.log(randomNumber1);
 console.log(randomNumber2);
+
 
 
 var changeableIMG1=document.querySelector('.img1');
@@ -51,4 +77,4 @@ switch (randomNumber2){
       break;
   default:
   console.log("error");
-}
+}*/
